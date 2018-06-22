f6217dd0379848f0c4d68fe533da28a937b8fcf9
Nikolajus Krauklis <nkrauklis@tesla.com>
2018-06-22 17:49:10 +0200

    intro clap

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,52 @@
 #![feature(integer_atomics)]
 #![feature(exclusive_range_pattern)]
 
-extern crate ignore;
 extern crate human_size;
+extern crate ignore;
 extern crate num_cpus;
+#[macro_use]
+extern crate clap;
 
+use clap::{App, Arg};
+use human_size::{Multiple, Size};
 use ignore::WalkBuilder;
-use std::sync::atomic::{AtomicU64, Ordering};
 use ignore::WalkState::*;
-use std::sync::{Arc, Mutex};
-use std::env::args;
-use std::collections::HashMap;
 use std::cmp;
-use human_size::{Size, Multiple};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let dir = match args().skip(1).next() {
-        Some(x) => x,
-        _ => ".".to_string(),
-    };
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about("ripdu is a tool to get your disk space back")
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name("FOLDER")
+                .help("Folder where to scan")
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .value_name("NUMBER")
+                .help("Number of top results to return. Default: 10"),
+        )
+        .get_matches();
+
+    let dir = matches.value_of("FOLDER").unwrap_or(".");
+    let results_amount = matches.value_of("number").unwrap_or("10");
+    let results_amount: usize = results_amount
+        .to_string()
+        .parse()
+        .expect("number value expected");
+
     let acc = Arc::new(AtomicU64::new(0));
     let files = Arc::new(Mutex::new(HashMap::new()));
+
+    println!("Scanning folder: {}", dir);
+
     let _ = WalkBuilder::new(dir)
         .ignore(false)
         .threads(num_cpus::get())
@@ -51,14 +77,14 @@ fn main() {
                                     data.insert(d, m.len());
                                 }
                                 acc_in.fetch_add(m.len(), Ordering::SeqCst);
-                            },
+                            }
                             Err(_) => return Continue,
                         };
                     };
                 } else {
                     return Continue;
                 }
-                
+
                 Continue
             })
         });
@@ -66,30 +92,37 @@ fn main() {
     let res = Arc::try_unwrap(acc).unwrap().into_inner();
     let all = Arc::try_unwrap(files).unwrap().into_inner().unwrap();
 
-    // for (_file, _size) in all.iter() {
-        // println!("{0:<020} {1}", get_human_readable_name(*_size), _file);
-    // }
-
     let mut count_vec: Vec<_> = all.iter().collect();
     count_vec.sort_by(|a, b| b.1.cmp(a.1));
-    for x in 0..cmp::min(10, count_vec.len()) {
-        println!("{} {}", get_human_readable_name(*count_vec[x].1), count_vec[x].0);
+    for x in 0..cmp::min(results_amount, count_vec.len()) {
+        println!(
+            "{} {}",
+            get_human_readable_name(*count_vec[x].1),
+            count_vec[x].0
+        );
+    }
+    if count_vec.len() > 0 {
+        println!("-------------");
     }
     println!("Total size of folder:\n {}", get_human_readable_name(res));
 }
 
 fn get_human_readable_name(size_in_bytes: u64) -> Size {
-    const KILO:u64 = 1024u64;
-    const MEGA:u64 = 1048576u64;
-    const GIGA:u64 = 1073741824u64;
-    const MAX:u64 = <u64>::max_value();
+    const KILO: u64 = 1024u64;
+    const MEGA: u64 = 1048576u64;
+    const GIGA: u64 = 1073741824u64;
+    const MAX: u64 = <u64>::max_value();
 
     match size_in_bytes {
         0..KILO => Size::new(_round(size_in_bytes as f64), Multiple::Byte).unwrap(),
-        KILO..MEGA => Size::new(_divide_and_round(size_in_bytes, KILO), Multiple::Kilobyte).unwrap(),
-        MEGA..GIGA => Size::new(_divide_and_round(size_in_bytes, MEGA), Multiple::Megabyte).unwrap(),
+        KILO..MEGA => {
+            Size::new(_divide_and_round(size_in_bytes, KILO), Multiple::Kilobyte).unwrap()
+        }
+        MEGA..GIGA => {
+            Size::new(_divide_and_round(size_in_bytes, MEGA), Multiple::Megabyte).unwrap()
+        }
         GIGA..MAX => Size::new(_divide_and_round(size_in_bytes, GIGA), Multiple::Gigabyte).unwrap(),
-        _ =>  Size::new(_round(size_in_bytes as f64), Multiple::Byte).unwrap(),
+        _ => Size::new(_round(size_in_bytes as f64), Multiple::Byte).unwrap(),
     }
 }
 
